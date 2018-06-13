3179f3d538c385c220a59e61901e12172e383b65
GeekTechDude <34283325+geektechdude@users.noreply.github.com>
2018-06-13 22:40:15 +0100

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,7 @@
+consumer_key        = ''
+consumer_secret     = ''
+access_token        = ''
+access_token_secret = ''
+
+#needs Twitter API keys from Twitter
+#keep the ' before and ' after the key is entered
